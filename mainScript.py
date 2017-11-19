8e79471585cf6620c68a8716e5fafa0f9558a9bd
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-11-19 14:31:28 +0100

    Added that the mainscript prints out the id of the recording.

--- mainScript.py
+++ mainScript.py
@@ -14,27 +14,16 @@ linkList.pop()
 
 a = linkList
 b = []
+
 for i in range(0,len(linkList)-1):
     filename = "emberiza-rustica_%d.mp3" %(i)
     proc = subprocess.Popen(['curl', '-Lo', filename, linkList[i]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
     b.append(re.findall("\d+", a[i]))
 
 
-print b[0][0]
+for i in range(0,len(linkList)-1):
+    print b[i][0]
 
-
-
-#print a
-
-
-#i=0
-#List=[]
-#for Line in tmp:
-#    List[i]=Line.split("\n")
-#    i+=1
-
-#print List
 
 #print p
 
